74fb2a6028cb725fef0a9f45caf015de3d5dd628
AlejandroNav <alejugger@gmail.com>
2020-06-28 19:17:21 -0500

    numeros y funciones de math

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
 print("Hello World")
 print("Bonjour la terre")
-nombre = "Juan"
-edad = "45"
-print("hola " + nombre + " es mi amigo de " + edad)
+nombre = "Juan Rofrigo marcano"
+edad = 45
+is_adulto = False
+print("hola " + nombre.upper() + " es mi amigo de " )
+
+print(nombre.islower())
+print(len(nombre))
+print(nombre[4])
+
+if is_adulto:
+    print('si lo es ' + nombre)
+else:
+    print("no lo es " + nombre)
