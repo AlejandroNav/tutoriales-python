15be1bb52925a98b6355c74b95ee1f21e03d7936
AlejandroNav <alejugger@gmail.com>
2020-07-03 14:44:37 -0500

    matrices terminado

---
+++ matriz.py
@@ -0,0 +1,12 @@
+numeros=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0],
+]
+print(numeros)
+print(numeros[2])
+print(numeros[2][1])
+for fila in numeros:
+    for columna in fila:
+        print(columna)
