376cc8a4afa9a7f191b242d47b56e498b2ab89c3
AlejandroNav <alejugger@gmail.com>
2020-07-02 20:19:34 -0500

    elif

--- statements.py
+++ statements.py
@@ -1,7 +1,11 @@
-es_hombre = False
+es_hombre = True
 is_tall = False
 
 if es_hombre and is_tall:
-    print("Eres hombre o alto o ambos")
+    print("Eres hombre alto")
+elif es_hombre and not(is_tall):
+    print("Eres un chaparro")
+elif not(es_hombre) and is_tall:
+    print("Eres una mujer alta")
 else:
-    print("Eres mujer o no eres alto")
+    print("Eres mujer chaparra")
