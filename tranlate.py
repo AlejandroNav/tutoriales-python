c95ecead55f0e6a3328f07bfbaebe2aa2dcf44e2
AlejandroNav <alejugger@gmail.com>
2020-07-03 15:38:36 -0500

    se agrega un archivo y se cominza  leerlo

--- tranlate.py
+++ tranlate.py
@@ -14,10 +14,22 @@ print(traducir("hola como estas"))
 print(traducir(input(" dame tu frase ")))
 #comentarios son para introducir informacion solo en codigo
 try:
-    value = 10/0
+    #value = 10/0
     number = int(input("dame un numero "))
     print(number)
 except ZeroDivisionError as err:
     print("err" + str(err))
 except ValueError as err:
     print("entrada invalida " + str(err))
+
+archivo_empleados = open("empleados.txt","r+")
+print(archivo_empleados.readable())  # te dice si es leible el archivo
+#print(archivo_empleados.read()) # te dice to do lo que hay dentro
+#print(archivo_empleados.readline()) # te da una linea
+empleados = archivo_empleados.readlines() # te da una linea
+print(empleados)
+
+for empledo in empleados:
+    print(str(empledo)+ " y trabaja desde hace")
+
+archivo_empleados.close()
