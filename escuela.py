3edd33a231127f0faa38cf9c95b23b2c5fe1e038
AlejandroNav <alejugger@gmail.com>
2020-07-03 19:51:51 -0500

    Cuestionarios con clases y multiples formularios

--- escuela.py
+++ escuela.py
@@ -9,13 +9,21 @@ print(estudiante2.nombre)
 preguntas = [
     "\nQue color es mas fuerte\n A) Azul\n B) Verde\n C) Naranja\n",
     "\nDonde esta La Paz\n A) Jamaica\n B) Bolivia\n C) Chile\n",
-    "\nQue tan alto es el Everest\n A) 4000m\n B) 566m\n C) 8850m\n"
+    "\nQue tan alto es el Everest\n A) 4000m\n B) 566m\n C) 8850m\n",
+    "\nQue es 5 + 5 \n A) 4000\n B) 5\n C) 10\n",
+    "\nQue tan alto eres\n A) 1.5\n B) 1,6\n C) 1,8\n",
+
 ]
 
-tabla = [
+tabla1 = [
     Cuestionario(preguntas[0], "A"),
     Cuestionario(preguntas[1], "B"),
     Cuestionario(preguntas[2], "C")
+]
+tabla2 = [
+    Cuestionario(preguntas[4],"C"),
+    Cuestionario(preguntas[3], "C"),
+    Cuestionario(preguntas[2], "C"),
 ]
 
 
@@ -28,4 +36,4 @@ def examen(tabla_entrada):
     print("Felicidades sacaste " + str(calificacion) + " de " + str(len(tabla_entrada)))
 
 
-examen(tabla)
+examen(tabla2)
