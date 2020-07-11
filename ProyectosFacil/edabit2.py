578946f6cdf63ff581dd27557d5d6aa38e15ff25
AlejandroNav <alejugger@gmail.com>
2020-07-10 23:39:38 -0500

    Dictionaries and module

--- ProyectosFacil/edabit2.py
+++ ProyectosFacil/edabit2.py
@@ -1,13 +1,44 @@
-def factorial(num):     # imprimir un factorial
+def factorial(num):  # imprimir un factorial
     suma = 1
-    for x in range(num): # por cada numero hasta llegar al numero dado
-        suma +=suma*x      # suma al contador la multiplicacion del numero * el contador
+    for x in range(num):  # por cada numero hasta llegar al numero dado
+        suma += suma * x  # suma al contador la multiplicacion del numero * el contador
     return suma
 
 
-def compound_interest(inversion, termino, interes, periodos): # in teres compuesto a 2 decimales
+def compound_interest(inversion, termino, interes, periodos):  # in teres compuesto a 2 decimales
     ans = inversion * (1 + interes / periodos) ** (periodos * termino)
     return round(ans, 2)
 
 
+def first_last(name):  # regresa el primer y ultimo caracter de un string concatenacion
+    return name[0] + name[-1]  # para acceder al ultimo elemneto se usa index negativo
+
+
+def oddeven(lst):  # decide si una lista tiene ams negativos o mas positivos
+    pares = 0
+    impares = 0
+    for numero in lst: # por cada numeros aumentas el contador
+        if not numero % 2:
+            pares += 1
+        else:
+            impares += 1
+    return pares < impares # evalua si hay mas pares que impares regresa Bool
+
+
+def mapping(letters):  # crea un diccioanrio con lo que le entra
+    dictionary = {}
+    for letter in letters:
+        dictionary[letter] = letter.upper() #en este casi la llave es la minuscula y el cntenido la mayuscula que creamos
+    return dictionary
+
+
+print(mapping(["p", "s","r", "w"]))
+l1 = [1, 2, 9, 9, 5, 9, 7, 9, 9]
+l2 = [4, 2, 4, 4, 5, 4, 7, 4, 2]
+if oddeven(l1):
+    print("hay mas pares")
+else:
+    print("hay mas impares")
+
+print(first_last("Hola cabeza"))
 print(factorial(7))
