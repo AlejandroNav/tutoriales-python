042119526f521a2b25a311988e4581faa93d6bb5
AlejandroNav <alejugger@gmail.com>
2020-07-19 20:00:36 -0500

    factorial recursive

--- ProyectosFacil/edabit4.py
+++ ProyectosFacil/edabit4.py
@@ -3,18 +3,18 @@ def mean(num):  # se promedian los digitos que entran como entero
     return sum(listnum) / len(listnum)
 
 
-def steps_to_convert(txt):    # regresa el menor numero de pasos para convertir a mayuscuals o minusculas
-    major = minor = 0  #declara dos variabels en cero
+def steps_to_convert(txt):  # regresa el menor numero de pasos para convertir a mayuscuals o minusculas
+    major = minor = 0  # declara dos variabels en cero
     for c in txt:
-        if c.isupper():     #si el character es mayuscula se suma major 1 else minor 1
+        if c.isupper():  # si el character es mayuscula se suma major 1 else minor 1
             major += 1
         else:
             minor += 1
-    return min(minor, major)    # regresa el menor de los contadores
+    return min(minor, major)  # regresa el menor de los contadores
 
 
-def sum_numbers(n): # suma natural usando recursividad
-    if n <= 1:      #si n es 1 termina el programa y regresa la sumatoria
+def sum_numbers(n):  # suma natural usando recursividad
+    if n <= 1:  # si n es 1 termina el programa y regresa la sumatoria
         return n
     return n + sum_numbers(n - 1)
 
@@ -22,6 +22,26 @@ def sum_numbers(n): # suma natural usando recursividad
 def remove_enemies(names, enemies):
     return [name for name in names if name not in enemies]
 
+
+def int_to_str(num):
+    return repr(num)
+
+
+def str_to_int(num):
+    return int(num)
+
+
+def fact(n):
+    if n <=1:
+        return 1
+    else:
+        if n <= 1:  # si n es 1 termina el programa y regresa la sumatoria
+            return n
+        return n * fact(n - 1)
+
+
+print(fact(6))
+print(int_to_str(567))
 
 print(remove_enemies(["Adam", "Emmy", "Tanya", "Emmy"], ["Emmy"]))
 
