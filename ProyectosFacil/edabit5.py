576a16471647f007ab55d0c8c858c2c60c69f5bb
AlejandroNav <alejugger@gmail.com>
2020-07-25 14:46:16 -0500

    cuadratic functions

---
+++ ProyectosFacil/edabit5.py
@@ -0,0 +1,20 @@
+import math
+
+
+def solutions(a, b, c): #A quadratic equation a x² + b x + c = 0 has either 0, 1, or 2 distinct solutions for real values of x.
+# Given a, b and c, you should return the number of solutions to the equation.
+    x=  b * b - (4*a*c)
+    if x < 0:
+        return 0
+    elif x > 0:
+        return 2
+    else:
+        return 1
+
+def quadratic_equation(a, b, c):
+    return ((b ** 2 - 4 * a * c) ** 0.5 - b) / 2 / a
+
+
+print(quadratic_equation(1, 2, -3)) # should be 1
+print(solutions(1, 0, -1)) #x² - 1 = 0 has two solutions (x = 1 and x = -1).
+
