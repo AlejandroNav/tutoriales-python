14acfbfbec7ce462c9cd20cd2c2b865419c0d62b
AlejandroNav <alejugger@gmail.com>
2021-02-01 12:20:07 -0600

    numbers

--- p1.py
+++ p1.py
@@ -1,20 +1,24 @@
+import math
+import random
 myName = "joven"
-print('What is your age?')    # ask for their age
+print('What is your age?')  # ask for their age
 myAge = input()
 if int(myAge) >= 70:
     print("The future is now old man")
     myName = "miedad"
 print(myName)
 
-
 frutero = ['apple', 'banana', 'cherry']
 for frutas in frutero:
     print(frutas)
 
-i = 1
-while i < 6:
-    print(i)
-    i += 1
+
+total = 0
+for numeros in range(101):
+    total = total + numeros
+print(total)
+
+
 
 i = 1
 while i < 6:
@@ -22,3 +26,6 @@ while i < 6:
     if i == 3:
         break
     i += 1
+
+print(random.randint(1,999))
+
--- tranlate.py
+++ tranlate.py
@@ -10,11 +10,12 @@ def traducir(frase):
             resultado = resultado + letra
     return resultado
 
+
 print(traducir("hola como estas"))
 print(traducir(input(" dame tu frase ")))
-#comentarios son para introducir informacion solo en codigo
+# comentarios son para introducir informacion solo en codigo
 try:
-    #value = 10/0
+    # value = 10/0
     number = int(input("dame un numero "))
     print(number)
 except ZeroDivisionError as err:
@@ -22,17 +23,15 @@ except ZeroDivisionError as err:
 except ValueError as err:
     print("entrada invalida " + str(err))
 
-archivo_empleados = open("empleados.txt","r+")
+archivo_empleados = open("empleados.txt", "r+")
 print(archivo_empleados.readable())  # te dice si es leible el archivo
-#print(archivo_empleados.read()) # te dice to do lo que hay dentro
-#print(archivo_empleados.readline()) # te da una linea
-empleados = archivo_empleados.readlines() # te da una linea
+# print(archivo_empleados.read()) # te dice to do lo que hay dentro
+# print(archivo_empleados.readline()) # te da una linea
+empleados = archivo_empleados.readlines()  # te da una linea
 print(empleados)
 archivo_empleados.write("\nDAniela - Chofera")
 
 for empledo in empleados:
-    print(str(empledo)+ " y trabaja desde hace")
+    print(str(empledo) + " y trabaja desde hace")
 
 archivo_empleados.close()
-
-while
