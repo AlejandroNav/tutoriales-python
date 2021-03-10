f221290129f3205a31ea7be472f23faf0c14396a
Alex-Nav <alejugger@gmail.com>
2021-03-10 11:36:24 -0600

    lists begins

--- p1.py
+++ p1.py
@@ -1,8 +1,11 @@
 import math
 import random
 myName = "joven"
+
 print('What is your age?')  # ask for their age
+
 myAge = input()
+
 if int(myAge) >= 70:
     print("The future is now old man")
     myName = "miedad"
@@ -19,7 +22,6 @@ for numeros in range(101):
 print(total)
 
 
-
 i = 1
 while i < 6:
     print(i)
@@ -27,8 +29,25 @@ while i < 6:
         break
     i += 1
 
-print(random.randint(1,999))
+print(random.randint(1, 999))
+
 
 def AgePlus(x):
     return x + 18
+
+
 print('Hello', ' your age is ', AgePlus(int(myAge)))
+
+
+def divBy10(divisor):
+    try:
+        return 10/divisor
+    except ZeroDivisionError:
+        return "no cero"
+
+print(divBy10(12))
+print(divBy10(10))
+print(divBy10(7))
+print(divBy10(5))
+print(divBy10(0))
+print(divBy10(120))
