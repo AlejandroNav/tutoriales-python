68a8c8abae611a127d356f8c839436182dd42ff6
Alex-Nav <alejugger@gmail.com>
2021-03-10 19:02:49 -0600

    finish lsit

---
+++ auto6.py
@@ -0,0 +1,17 @@
+# Capitulo 6 listas
+spam = ['cat', 'bat', 'rat', 'elephant','jmaon','amigos','aguila']
+
+# negativo se refiere al ultimo
+print(spam[-1])
+
+# slice can get several items froma list where starts and : stop before
+print(spam[1:4])
+
+for item in range(3,5):
+    print(item)
+
+# augmented assigned operator
+
+bumi = 45
+bumi += 1
+ 
