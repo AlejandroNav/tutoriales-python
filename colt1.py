4399cc13f7bd092eec7859f4144da2c2ec79c09e
Alex-Nav <alejugger@gmail.com>
2021-03-24 21:36:14 -0600

    loops

--- colt1.py
+++ colt1.py
@@ -5,11 +5,12 @@ print('love')
 
 player1 = input('Player 1 dame tu movimiento ')
 player2 = input('Player 2 dame tu movimiento ')
-print(random.randint(0,2))
+print(random.randint(0, 2))
 
-if player1==player2:
+if player1 == player2:
     print("empate con " + player1)
 elif player1 == "rock":
+
     if player2 == "love":
         print("Jugador 1 gana con " + player1)
     elif player2 == "paper":
@@ -24,3 +25,15 @@ elif player1 == "love":
         print("Jugador 1 gana con " + player1)
     elif player2 == "paper":
         print("Jugador 2 gana con " + player2)
+
+for item in range(1, 21):
+
+    print(item)
+
+for item in range(1, 10):
+    print("\U0001f600"*item)
+
+times = 11
+while times >= 1:
+    print("\U0001f600"*item)
+    times -=1
