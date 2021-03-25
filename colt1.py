182c16909cbb676d714dd5457ee732b200e0abca
Alex-Nav <alejugger@gmail.com>
2021-03-24 19:01:57 -0600

    rando rock paper

--- colt1.py
+++ colt1.py
@@ -1,4 +1,26 @@
-guess = 9
-print(f'you gues of {guess} as incorrect')
+import random
+print('rock')
+print('paper')
+print('love')
 
-print
+player1 = input('Player 1 dame tu movimiento ')
+player2 = input('Player 2 dame tu movimiento ')
+print(random.randint(0,2))
+
+if player1==player2:
+    print("empate con " + player1)
+elif player1 == "rock":
+    if player2 == "love":
+        print("Jugador 1 gana con " + player1)
+    elif player2 == "paper":
+        print("Jugador 2 gana con " + player2)
+elif player1 == "paper":
+    if player2 == "rock":
+        print("Jugador 2 gana con " + player2)
+    elif player2 == "love":
+        print("Jugador 1 gana con " + player1)
+elif player1 == "love":
+    if player2 == "rock":
+        print("Jugador 1 gana con " + player1)
+    elif player2 == "paper":
+        print("Jugador 2 gana con " + player2)
